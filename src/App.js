7fde6c17ca6c41ff540a2e194666fd8e44ca7ba1
Sinestro38 <pavanjayasinha@gmail.com>
2020-09-21 08:40:48 -0400

    commit on 'master'

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Fragment, Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+
+  render() {
+    const name = 'John Doe';
+    const loading = false;
+    const showName = true;
+
+    return (
+      <div className='App'>
+        {loading ? <h3>Loading...</h3> : <h1>Hello {showName ? name : null}</h1>}
+      </div>
+    );
+  }
 }
 
 export default App;
