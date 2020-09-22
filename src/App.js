8e6fb4276b4c2933c902d834c34f1b94b7de3e23
Sinestro38 <pavanjayasinha@gmail.com>
2020-09-22 11:59:27 -0400

    repo

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
-import React, { Fragment, Component } from 'react';
+import React, { Component } from 'react';
+import Navbar from './components/layout/Navbar';
 import './App.css';
+import axios from 'axios';
+import Users from './components/users/Users';
+import Search from './components/users/Search';
 
 class App extends Component{
+  state = {
+    users: [],
+    loading: false
+  }
+
+  async componentDidMount() {
+    this.setState({loading: true});
+    const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+    this.setState({users: res.data, loading: false});
+  }
+
+  searchUsers= text => {
+    console.log(text);
+  }
 
   render() {
-    const name = 'John Doe';
-    const loading = false;
-    const showName = true;
-
     return (
       <div className='App'>
-        {loading ? <h3>Loading...</h3> : <h1>Hello {showName ? name : null}</h1>}
+          <Navbar />
+
+          <div className="container">
+            < Search searchUsers={this.searchUsers} />
+            <Users users={this.state.users} loading={this.state.loading} />
+          </div>
       </div>
+
     );
   }
 }
