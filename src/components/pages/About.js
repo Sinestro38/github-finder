2eff65f03316f9cee874d404b151a6aff189d047
Sinestro38 <pavanjayasinha@gmail.com>
2020-09-24 19:53:25 -0400

    commit on master

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -6,6 +6,7 @@ const About = () => {
             <h1>About This App</h1>
             <p>App to search Github users</p>
             <p>Version: 1.0.0</p>
+            <p>Made by Pavan</p>
         </Fragment>
     )
 }
